7b87c059fb94300129c6de785b33b0fd291c1bb5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-11 04:50:46 -0700

    Add if statement to prevent executing from terminal and only executes if imported from a module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-args = sys.argv[1:]
-i = 1
-arguments = "arguments" if len(args) > 1 else "argument"
-if len(args) > 0:
-    print("{} {}:".format(len(args), arguments))
-    for arg in args:
-        print("{}: {}".format(i, sys.argv[i]))
-        i = i + 1
-else:
-    print("0 arguments.")
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    i = 1
+    arguments = "arguments" if len(args) > 1 else "argument"
+    if len(args) > 0:
+        print("{} {}:".format(len(args), arguments))
+        for arg in args:
+            print("{}: {}".format(i, sys.argv[i]))
+            i = i + 1
+        else:
+            print("0 arguments.")
