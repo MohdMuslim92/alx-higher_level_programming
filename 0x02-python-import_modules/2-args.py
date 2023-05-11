c752dd348b49e63a1749d676f5a62a484807e32d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-11 03:16:45 -0700

    Prints programs arguments (argv) to the terminal

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+args = sys.argv[1:]
+i = 1
+arguments = "arguments" if len(args) > 1 else "argument"
+if len(args) > 0:
+    print("{} {}:".format(len(args), arguments))
+    for arg in args:
+        print("{}: {}".format(i, sys.argv[i]))
+        i = i + 1
+else:
+    print("0 arguments.")
