19d67c6e74810d2408dd78e2d0a05e92a4140c46
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-11 19:39:13 -0700

    Edit-code style

--- 0x02-python-import_modules/upper.py
+++ 0x02-python-import_modules/upper.py
@@ -1,5 +1,7 @@
 import string
-def print_upper_letters(index = 0):
-    print(string.ascii_uppercase[index], end = "")
+
+
+def print_upper_letters(index=0):
+    print(string.ascii_uppercase[index], end="")
     if index < 25:
         print_upper_letters(index + 1)
