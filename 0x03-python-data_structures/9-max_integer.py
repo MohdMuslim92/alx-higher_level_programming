e5d504d117735aa78f9c14c21de935eb24f39482
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-12 10:45:37 -0700

    Function that receive a list and return the biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    else:
+        my_list.sort()
+        maximum = my_list[-1]
+    return maximum
