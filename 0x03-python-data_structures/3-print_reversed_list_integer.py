9dbf7a359de57d7fa32d30c466a86402390886d6
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-13 05:52:10 -0700

    Edit - remove casting integers into strings

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     reversed_list = my_list.copy()
     reversed_list.reverse()
     for item in reversed_list:
-        print("{}".format(int(item)))
+        print("{:d}".format(item))
