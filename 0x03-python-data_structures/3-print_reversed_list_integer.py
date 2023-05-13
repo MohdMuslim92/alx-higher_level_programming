e2ecd005f4bbc6cd2ef840969d9811fd8a87ad9e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-13 06:24:59 -0700

    Revert "Edit - if empty list passed just return from the funcion"
    This reverts commit a33cbb95f125fe74b1936e3306ef27caeec4b72c.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    if length == 0:
-        print('[]')
-    else:
-        reversed_list = my_list.copy()
-        reversed_list.reverse()
-        for item in reversed_list:
-            print("{:d}".format(item))
+    reversed_list = my_list.copy()
+    reversed_list.reverse()
+    for item in reversed_list:
+        print("{:d}".format(item))
