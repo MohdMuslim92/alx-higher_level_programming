a33cbb95f125fe74b1936e3306ef27caeec4b72c
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-13 06:23:01 -0700

    Edit - print empty list if the passed list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list.copy()
-    reversed_list.reverse()
-    for item in reversed_list:
-        print("{:d}".format(item))
+    length = len(my_list)
+    if length == 0:
+        print('[]')
+    else:
+        reversed_list = my_list.copy()
+        reversed_list.reverse()
+        for item in reversed_list:
+            print("{:d}".format(item))
