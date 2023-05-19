0fc10e8052f1f0a94d3bbb4687fcbad896fad356
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-19 06:23:26 -0700

    Fix - if the new node is smaller than first node then insert at beginning, and if we reached the end of linked list then insert at the end

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -26,9 +26,15 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = node;
 		return (node);
 	}
+	if (current->n > number)
+	{
+		node->next = current;
+		*head = node;
+		return (node);
+	}
 	while (current != NULL)
 	{
-		if (current->next->n > number)
+		if (current->next->n > number || current ->next == NULL)
 		{
 			temp = current->next;
 			current->next = node;
