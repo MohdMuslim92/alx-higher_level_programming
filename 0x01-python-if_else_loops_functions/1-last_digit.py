72dc5dadebffc832fd49426574558297c0adbdd4
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-19 15:57:06 -0700

    Fix correct answer if the number is a minus

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    sign = -1
+else:
+    sign = 1
 last_digit = abs(number) % 10
+last_digit  = last_digit * sign
 string = "Last digit of"
 if last_digit > 5:
     print(f"{string} {number:d} is {last_digit} and is greater than 5")
