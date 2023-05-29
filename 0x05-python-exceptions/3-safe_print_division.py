9aa621b0e54696b5ea5e6abb0e8f67ea2a061580
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-05-29 16:46:06 +0000

    Modify 3-safe_print_division.py to print float number
    - Updated the file 3-safe_print_division.py.
    - Modified the function to print the division result as a float number with a single decimal point.
    - The result is now formatted using "{:.1f}".format() to display one decimal place.
    - This change improves the output format, providing a more concise representation of the float number.
    
    The modification ensures that the division result is printed as a float number with one decimal point, improving the readability of the output.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
         result = None
     finally:
         if result is not None:
-            print("Inside result: {:f}".format(result))
+            print("Inside result: {:.1f}".format(result))
         else:
             print("Inside result: None")
     return result
