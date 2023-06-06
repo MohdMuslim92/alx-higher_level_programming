7c1e8603f3aa1a1bf35399e8b5fb11a5a734fd35
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-06 00:12:43 +0000

    Created the file 101-nqueens.py
    - A program that solves the N-Queens problem.
    - The N-Queens problem involves placing N non-attacking queens on an N x N chessboard.
    - The program takes a single command-line argument, N, representing the size of the chessboard and the number of queens.
    
    - The value of N must be an integer greater than or equal to 4.
    - If the user provides the wrong number of arguments or an invalid value for N, appropriate error messages will be displayed.

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+import sys
+
+
+def move(n):
+    result = []
+    backtrack(n, [], result)
+    return result
+
+
+def backtrack(n, path, result):
+    if len(path) == n:
+        result.append(path[:])
+        return
+
+    for col in range(n):
+        if is_safe(col, path):
+            path.append(col)
+            backtrack(n, path, result)
+            path.pop()
+
+
+def is_safe(col, path):
+    row = len(path)
+    for r, c in enumerate(path):
+        if c == col or r - c == row - col or r + c == row + col:
+            return False
+    return True
+
+
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+n = sys.argv[1]
+
+try:
+    n = int(n)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+moves = move(n)
+for movement in moves:
+    print(movement)
