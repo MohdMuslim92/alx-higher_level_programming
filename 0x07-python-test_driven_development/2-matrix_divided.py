22abfe64dab700784dd925865486af292d521ad3
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-07 01:02:31 +0000

    Edit pycodestyle and added docstring for module

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""This module createsva function that divide a matrix
+by a number"""
+
+
 def matrix_divided(matrix, div):
+    """Function that divide every element in a matrix
+    by a divisor passed to it"""
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                (list of lists) of integers/floats")
     row_lengths = []
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of     integers/floats")
+            raise TypeError("matrix must be a matrix \
+                    (list of lists) of     integers/floats")
         row_lengths.append(len(row))
         if len(set(row_lengths)) != 1:
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of     integers/floats")
+                raise TypeError("matrix must be a matrix \
+                        (list of lists) of     integers/floats")
             if not isinstance(div, (int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
