479fec8965638d108dab564f8391cb00004d3fe5
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-07 08:59:41 +0000

    Added function for text indentation
    - This commit introduces a new function named text_indentation that prints a given text with two new lines after each occurrence of the characters ".", "?", and ":".
    - The function takes a single argument, text, which must be a string.
    
    - The function performs a type check to ensure that the input text is a string.
    - If it is not, a TypeError exception is raised.
    
    - The function iterates over each character in the text and checks for the characters ".", "?", and ":".
    If any of these characters are found, two new lines are printed.
    Additionally, any leading or trailing spaces in each printed line are eliminated.
    
    - The flag variable is used to ensure that only two new lines are printed after each occurrence of the specified characters.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module creates a function that prints a text with 2
+new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """Function that prints a newline after every ., ? and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    flag = 0
+    for c in text:
+        if c == "." or c == "?" or c == ":":
+            print("\n")
+            flag = 1
+            continue
+        if flag == 1:
+            flag = 0
+            continue
+        print(c, end="")
+    print()
