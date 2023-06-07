1bbccc1a4212af46e9fe0b8fb60426988784c9b7
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-07 11:35:48 +0000

    Edit code indentation, remove extra tabs from raise message

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,14 +13,14 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if not isinstance(row, list):
             raise TypeError("matrix must be a matrix \
-                    (list of lists) of     integers/floats")
+                    (list of lists) of integers/floats")
         row_lengths.append(len(row))
         if len(set(row_lengths)) != 1:
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
                 raise TypeError("matrix must be a matrix \
-                        (list of lists) of     integers/floats")
+                        (list of lists) of integers/floats")
             if not isinstance(div, (int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
