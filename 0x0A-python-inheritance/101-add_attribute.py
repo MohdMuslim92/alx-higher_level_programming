1f93d2e8afc228c8ee778ab790bd42e251db2e32
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-13 00:33:42 +0000

    Update 101-add_attribute add docstring to the function

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,9 @@
 
 
 def add_attribute(obj, name, value):
+    """Function that adds a new attribute to an object if
+    it's possible, it Raise a TypeError exception if the 
+    object can't have new attribute"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     obj.__dict__[name] = value
