6f792c12113aef850c1dfe0e734940709e27cfa8
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-13 00:44:18 +0000

    Update 7-base_geometry.py add docstring to the methods

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,12 +11,28 @@ class BaseGeometry(object):
     less or equal to 0: raise a ValueError exception."""
 
     def __init__(self):
+        """Initialize the BaseGeometry instance."""
         pass
 
     def area(self):
+        """Compute the area of the geometry.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate an integer value.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
