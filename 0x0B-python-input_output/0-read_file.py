435a7ae2a13351dca2e66924e124585e21ff79d1
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-14 15:58:12 +0000

    Update 0-read_file.py using approach of reading files in chunks to increase memory efficiency

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,9 @@ def read_file(filename=""):
     """Function that reads a text file (UTF8) and prints it to stdout:
     using with statement"""
     with open(filename, encoding="UTF8") as my_file:
-        lines = my_file.read().rstrip()
-    if lines:
-        print(lines)
+        while True:
+            lines = my_file.read(4096).rstrip()
+            if not lines:
+                break
+            if lines:
+                print(lines)
