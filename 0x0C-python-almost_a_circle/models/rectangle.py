8fc74cf98f34fcae86848715f4d21a174f24e68e
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-16 01:21:50 +0000

    Update models/rectangle.py
    - Improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,8 +103,10 @@ class Rectangle(Base):
 
     def display(self):
         """Print the rectangle using the character '#'."""
+        for i in range(self.y):
+            print()
         for i in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Return a string representation of the rectangle."""
