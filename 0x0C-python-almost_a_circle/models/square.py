d0e23215a81c9b703f7b02ee6f9842bd526a1416
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-16 02:29:11 +0000

    reate models/square.py
    - A class Square that inherits from Rectangle:
    Class constructor: def __init__(self, size, x=0, y=0, id=None)::
        Call the super class with id, x, y, width and height - this super call will use the logic of the __init__ of the Rectangle class. The width and height must be assigned to the value of size
        a Rectangle with the same width and height
        All width, height, x and y validation inherit from Rectangle - same behavior in case of wrong data
        The overloading __str__ method returns [Square] (<id>) <x>/<y> - <size> - in our case, width or height

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+"""This module defines a class Square that inherits from Rectangle"""
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from the Rectangle class.
+
+    Attributes:
+        size (int): Size of the square.
+        x (int): X-coordinate of the square's position.
+        y (int): Y-coordinate of the square's position.
+        id (int): Unique identifier for the square.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize the Square object.
+
+        Args:
+            size (int): Size of the square.
+            [optional] x (int): X-coordinate of the square's position.
+            Defaults to 0.
+            [optional] y (int): Y-coordinate of the square's position.
+            Defaults to 0.
+            [optional] id (int): Unique identifier for the square.
+            Defaults to None.
+
+        Note:
+            The id, x, y, width, and height arguments are passed to
+            the super class constructor (Rectangle) using the super()
+            function. The width and height are assigned the value of
+            size.
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return "[square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
