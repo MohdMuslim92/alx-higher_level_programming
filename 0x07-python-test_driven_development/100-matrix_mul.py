865f2ab11345bb6a1fb741a02e672d59cf337871
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-19 19:33:37 +0000

    Update 100-matrix_mul.py separate checks for m_a and m_b list

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -25,8 +25,10 @@ def matrix_mul(m_a, m_b):
     """
 
     # Validate m_a and m_b are lists
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a must be a list or m_b must be a list")
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
 
     # Validate m_a and m_b are lists of lists
     if not all(isinstance(row, list) for row in m_a):
