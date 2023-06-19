0a87be3a63f042b585537a85578c14cf479d4d2c
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-19 08:49:53 +0000

    Add unit tests for the max_integer function

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+"""
+Unit tests for the max_integer function.
+
+This module contains test cases for the max_integer function
+from the '6-max_integer' module. It tests various scenarios
+to ensure the correct behavior of the max_integer function.
+"""
+
+import unittest
+from importlib import import_module
+
+max_integer = import_module('6-max_integer').max_integer
+
+
+class MaxIntegerTest(unittest.TestCase):
+    """
+    Test case class for testing the max_integer function.
+    """
+
+    def test_empty_list(self):
+        """
+        Test the max_integer function with an empty list.
+        The expected result is None.
+        """
+
+        result = max_integer([])
+        self.assertIsNone(result)
+
+    def test_single_element(self):
+        """
+        Test the max_integer function with a list containing a single element.
+        The expected result is the same single element.
+        """
+
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
+    def test_positive_numbers(self):
+        """
+        Test the max_integer function with a list of positive numbers.
+        The expected result is the maximum number from the list.
+        """
+
+        result = max_integer([1, 2, 3, 4, 5])
+        self.assertEqual(result, 5)
+
+    def test_negative_numbers(self):
+        """
+        Test the max_integer function with a list of negative numbers.
+        The expected result is the maximum number from the list.
+        """
+
+        result = max_integer([-5, -4, -3, -2, -1])
+        self.assertEqual(result, -1)
+
+    def test_mixed_numbers(self):
+        """
+        Test the max_integer function with a list of mixed positive
+        and negative numbers.
+        The expected result is the maximum number from the list.
+        """
+
+        result = max_integer([-5, 0, 5, -10, 10])
+        self.assertEqual(result, 10)
+
+    def test_duplicate_numbers(self):
+        """
+        Test the max_integer function with a list of duplicate numbers.
+        The expected result is the same duplicate number.
+        """
+
+        result = max_integer([5, 5, 5, 5])
+        self.assertEqual(result, 5)
+
+    def test_large_list(self):
+        """
+        Test the max_integer function with a large list of numbers.
+        The expected result is the maximum number from the list.
+        """
+
+        result = max_integer(list(range(1000000, 0, -1)))
+        self.assertEqual(result, 1000000)
+
+
+if __name__ == '__main__':
+    unittest.main()
