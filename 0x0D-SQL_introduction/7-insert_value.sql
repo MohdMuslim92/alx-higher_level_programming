a8503ddc400b43e9ac4a6b75354bfd01cd8d8c1c
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-07-18 14:17:55 -0700

    Update 7-insert_value.sql
    - Remove database name from the script as it will be passed as a
    parameter with the mysql command

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,5 +2,5 @@
 -- first_table (database hbtn_0c_0) in a MySQL server
 
 -- INSERT statement is used for inserting rows in a mysql table
-INSERT INTO hbtn_0c_0.first_table (id, name)
+INSERT INTO `first_table` (id, name)
 VALUES (89, 'Best School');
