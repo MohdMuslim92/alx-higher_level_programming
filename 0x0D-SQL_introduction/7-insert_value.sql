242f96c2f30492dcd86acc48bd5506dbb948103b
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-07-18 14:12:31 -0700

    Create 7-insert_value.sql
    - Insert row in a mysql database

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Script that inserts a new row in the table
+-- first_table (database hbtn_0c_0) in a MySQL server
+
+-- INSERT statement is used for inserting rows in a mysql table
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
