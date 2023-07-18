bc55b5ba6dd2eb81952a7b8ef69c7300d9af8b0c
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-07-18 11:18:41 -0700

    Create 6-list_values.sql that displays rows of a table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- Script that lists all rows of the table first_table from
+-- the database hbtn_0c_0 in a MySQL server
+
+-- SELECT statement is used for displaying the rows of a table
+SELECT *
+FROM `hbtn_0c_0`.`first_table`;
