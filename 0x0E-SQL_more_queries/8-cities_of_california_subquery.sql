532c6bafb36ec688c2d9dbda17b2d17eb739d27a
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-07-19 14:41:03 -0700

    Update 8-cities_of_california_subquery.sql
    - Remove the state_id column from the result

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- be found in the database hbtn_0d_usa
 -- The states table contains only one record where name = California (but the id can be different
 -- Results are sorted in ascending order by cities.id
-SELECT *
+SELECT id, name
 FROM cities
 WHERE state_id = (
 	SELECT id
