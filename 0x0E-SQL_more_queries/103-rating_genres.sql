ef1c6f83336e802ef10447a260e8a4d45c7e4c2e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-07-19 17:32:13 -0700

    Update 103-rating_genres.sql
    - Remove second SELECT as per requirements

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,10 +3,7 @@
 -- Results are sorted in ascending order by the rating
 SELECT tv_genres.name, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
 FROM tv_genres
-LEFT JOIN  (
-	SELECT tv_show_genres.genre_id, hbtn_0d_tvshows_rate.rating
-	FROM tv_show_genres
-	JOIN hbtn_0d_tvshows_rate ON tv_show_genres.show_id = hbtn_0d_tvshows_rate.show_id
-) AS genre_ratings ON tv_genre.id = genre_ratings.genre_id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN hbtn_0d_tvshows_rate ON tv_show_genres.show_id = hbtn_0d_tvshows_rate.show_id
 GROUP BY tv_genres.name;
 ORDER BY rating_sum DESC;
