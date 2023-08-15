2bfb4fa4b825199bc1731440c20c9cde9d2b3dc3
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-15 03:55:56 +0300

    Update 5-to_integer.js
    - Fix the correct output when the command argument is 0 it should
    consider it a number using the function isInteger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const integerNumber = parseInt(args[0]);
-if (integerNumber) {
+if (Number.isInteger(integerNumber)) {
   console.log('My number: ', args[0]);
 } else {
   console.log('Not a number');
