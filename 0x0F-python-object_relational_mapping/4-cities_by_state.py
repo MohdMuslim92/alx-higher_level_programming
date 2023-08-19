f69ff2b62bb189f86fc14d061eafa39a8cf065ef
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-20 01:58:59 +0300

    Create 4-cities_by_state.py
    - Script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves all cities from the
+'cities' table.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """
+    Main function that connects to the MySQL database and retrieves all cities.
+    """
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <db_name> <state_name>"
+              .format(argv[0]))
+        return
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    con = None
+    cursor = None
+    try:
+        con = MySQLdb.connect(
+                host="localhost",
+                port=3306, user=user, password=password, db=db_name)
+        cursor = con.cursor()
+        query = "SELECT cities.id, cities.name " \
+                "FROM cities " \
+                "INNER JOIN states ON cities.state_id = states.id " \
+                "ORDER BY states.id ASC"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error connecting to the MySQL server: ", e)
+    finally:
+        if cursor:
+            cursor.close()
+        if con:
+            con.close()
+
+
+if __name__ == "__main__":
+    main()
