498e9866e8399578fc5174a826cc666ed7338ec5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-20 02:03:37 +0300

    Update 4-cities_by_state.py
    - Display the states name
    - Order cities by cities_id instead of states_id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -27,10 +27,10 @@ def main():
                 host="localhost",
                 port=3306, user=user, password=password, db=db_name)
         cursor = con.cursor()
-        query = "SELECT cities.id, cities.name " \
+        query = "SELECT cities.id, cities.name, states.name " \
                 "FROM cities " \
                 "INNER JOIN states ON cities.state_id = states.id " \
-                "ORDER BY states.id ASC"
+                "ORDER BY cities.id ASC"
         cursor.execute(query)
         rows = cursor.fetchall()
         for row in rows:
