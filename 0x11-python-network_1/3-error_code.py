90bd11be49e73bdda6792de08a801796aedf2b4e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-01 14:15:50 +0300

    Create 3-error_code.py
    - Script thatsends a request to a URL and displays the body of the response
      (decoded in utf-8).
    - It handles urllib.error.HTTPError exceptions and prints the HTTP status code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script sends a request to a URL and displays the body of the response
+(decoded in utf-8).
+It handles urllib.error.HTTPError exceptions and prints the HTTP status code.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: {} <URL>\n".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as e:
+        sys.stderr.write("Error code: {}\n".format(str(e.code)))
+        sys.exit(1)
+    except Exception as e:
+        sys.stderr.write("Error: {}\n".format(str(e)))
+        sys.exit(1)
